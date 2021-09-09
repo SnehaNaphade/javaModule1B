c87346cd83cbe41ce331d56efb0443e37d8dcd31
Sneha Naphade <sneha.naphade833@gmail.com>
2021-09-09 14:54:33 +0530

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+ class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println();
+        System.out.println();
+    }
+
+}
+
+// Make project
+// create class
+// make main method
+// print something from it
