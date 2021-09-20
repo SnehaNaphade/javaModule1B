933e265f137f019cea2469685479d5d5b6d85d63
Sneha Naphade <89774976+SnehaNaphade@users.noreply.github.com>
2021-09-20 16:42:16 +0530

    Update useBox.java

--- src/Classes/useBox.java
+++ src/Classes/useBox.java
@@ -3,15 +3,12 @@ package Classes;
 class UseBox{
     public static void main(String[] args) {
 
-        //Create object of class this way
         Box a = new Box(10,10,10,10);
         Box b = new Box(5,10,5,10);
 
         double volume1, volume2;
 
-        //Assign Values
-
-
+   
         volume1 = a.volume();
         volume2 = b.volume();
 
